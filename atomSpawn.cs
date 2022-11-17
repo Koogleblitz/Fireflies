203fd7744369b6bb85402763eff3de35f0ab690d
Richard Tobing <richardtobing@outlook.com>
2022-11-16 21:07:42 -0800

    atoms seem to spawn; has position and its data can be seen in the enity debugger, but appears invisible when playing the game

--- atomSpawn.cs
+++ atomSpawn.cs
@@ -45,10 +45,14 @@ public class atomSpawn : MonoBehaviour
     {
         population = 0;
         tick= 0;
+
+        //Create entity manager, each world has one entity manager
         entMgmt= World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        //the postition of the spawner object that this script will attach to, atoms will spawn at this position
         position= this.transform.position;
 
-        // -- Create archetype with System.Type of the enties' dynamic fields ---
+        // -- Create archetype with System.Type of the enties' dynamic fields, archetypes = "entity template" ---
         atomBox= entMgmt.CreateArchetype(            
             typeof(Translation),
             typeof(Rotation),
@@ -56,7 +60,7 @@ public class atomSpawn : MonoBehaviour
             typeof(RenderMesh),
             typeof(RenderBounds),
             typeof(atomFields)
-        );
+            );
     }
 
 
@@ -66,15 +70,19 @@ public class atomSpawn : MonoBehaviour
             tick+= 1;
         }else{
             tick= 0;
-
             for(int i=0 ; i<= spawnRate; i++){
                 if(population <= populationControl){
+
+                    //This is he 'entity assembly line' in which each atom will be created and 'processed' before spawning
                     Entity atom= entMgmt.CreateEntity(atomBox);
 
-                    entMgmt.AddComponentData(atom, new Translation{Value = position});
+                    //Decide the spawn location (same location as teh spawner itself):
+                    entMgmt.AddComponentData(atom, new Translation{
+                        Value = position
+                        });
 
 
-
+ 
                     entMgmt.AddComponentData(atom, new atomFields{
                         centroidGrav_ui= centroidGrav_ui,
                         socialDistance_ui= socialDistance_ui,
@@ -88,35 +96,32 @@ public class atomSpawn : MonoBehaviour
                         posIncrement= posIncrement, 
                         playerShip= playerShip,
                         sector= sector
-                    });
+                        });
 
 
+
+                    // get material, 'enable gpu instancing' in inspector for that material
+                    // give the entity material and a mesh to make it visible
                     entMgmt.AddSharedComponentData(atom, new RenderMesh{
-                        // get material, 'enable gpu instancing' in inspector for that material
-                        // assign the spawn script to an empty object
-                        //drag the mesh and material to the spawn script in the inspector                        
-
-
-                        // material= 
-                        // mesh= 
-                    });
+                        material= material,
+                        mesh= mesh,
+                        //castShadows = UnityEngine.Rendering.ShadowCastingMode.On
+                        });
+                    
 
 
 
 
 
-
-
-
-
-
-
-
-
-
+                population= population+1;
 
                 }
+               
             }
         }
     }
+
+
+
+
 }
